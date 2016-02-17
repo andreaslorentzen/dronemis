8c94920bd2ed22513489092891c59afd6597f194
hippomormor <cgenter@oncable.dk>
2016-02-17 16:36:11 +0100

    Automatic check for Mac/Windows

--- src/dronemis/openCV.java
+++ src/dronemis/openCV.java
@@ -4,22 +4,30 @@ package dronemis;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
+import java.net.URL;
 
-/**
- *
- * @author AL
- */
 public class openCV {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
 
-        String libopencv_java = "/Users/hippomormor/Desktop/dronemis/lib/openCV/java/mac/libopencv_java310.dylib";
-        System.load(libopencv_java);
-        Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
-        System.out.println( "mat = " + mat.dump() );
+        System.load(getLibPath());
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("mat = " + mat.dump());
     }
 
+    private static String getLibPath() {
+
+        final String dir = System.getProperty("user.dir");
+        String OS = System.getProperty("os.name").toLowerCase();
+
+        if (OS.contains("windows"))
+            return dir + "\\lib\\openCV\\java\\x64\\opencv_java310.dll";
+        else if (OS.contains("mac"))
+            return dir + "/lib/openCV/java/mac/libopencv_java310.dylib";
+        else
+            System.err.println("Unable to determine OS!");
+
+        return null;
+    }
 }
