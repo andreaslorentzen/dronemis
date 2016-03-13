eaea7f37428473f453880d8914446650e63606ce
hippomormor <cgenter@oncable.dk>
2016-03-13 16:57:26 +0100

    GUI

--- src/dronemis/Main.java
+++ src/dronemis/Main.java
@@ -11,7 +11,6 @@ import java.io.IOException;
  * Created by AL on 02-03-2016.
  */
 public class Main {
-
     public static void main(String[] args) throws IOException {
         new Controller().startSystem();
 
@@ -23,4 +22,5 @@ public class Main {
             listener.updateImage(image);
         }
     }
+
 }
